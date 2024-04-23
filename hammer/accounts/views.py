9d7a5047a5c0ef4b25224d94e46dfcce78cfdfc1
Michael-Zapivahin <Zapivahin@ya.ru>
2024-04-23 12:53:35 +0300

    start 3

---
+++ hammer/accounts/views.py
@@ -0,0 +1,17 @@
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
+from .forms import CustomUserCreationForm
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("home")
