11c881b54d6bb2011bc61ef56778a141bbc2eb79
Michael-Zapivahin <Zapivahin@ya.ru>
2024-04-23 12:52:17 +0300

    start 2

--- hammer/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import SignUpView, user_logout
-
-urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
-    path("logout/", user_logout, name="signup"),
-]
